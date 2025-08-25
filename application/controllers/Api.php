56fde73fab04c05a1f3348dfd6df202d280e3d5d
prayuda wirawan <prayudaw@gmail.com>
2025-08-25 15:47:00 +0700

    membuat endpoint get mahasiswa

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -9,6 +9,7 @@ class Api extends CI_Controller
         parent::__construct();
         // Memuat model jika diperlukan, misalnya untuk data pengguna
         $this->load->model('anggota_model');
+        $this->load->model('tansaksi_model');
     }
 
     public function index()
@@ -23,23 +24,24 @@ class Api extends CI_Controller
             ->set_output(json_encode($response));
     }
 
-    public function getUser($nim)
-    {
-        // Contoh endpoint untuk mendapatkan data pengguna
-        $data_anggota = $this->anggota_model->get_data_anggota($id);
 
-        if ($user) {
-            $response = [
+    // endpoint untuk mendapatkan data anggota
+    public function get_mahasiswa()
+    {
+        $nim = $this->input->get('nim');
+        $data_anggota = $this->anggota_model->get_data_anggota($nim);
+
+        if ($data_anggota) {
+            $response = array(
                 'status' => 'success',
                 'data' => $data_anggota
-            ];
+            );
         } else {
-            $response = [
+            $response = array(
                 'status' => 'error',
                 'message' => 'Pengguna tidak ditemukan.'
-            ];
+            );
         }
-
         $this->output
             ->set_content_type('application/json')
             ->set_output(json_encode($response));
